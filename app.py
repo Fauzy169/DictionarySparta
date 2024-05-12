6a36c11e32e90cea60d378fbca6e55ce34257c94
Fauzy169 <ahmadfauzy169@gmail.com>
2024-05-12 23:48:56 +0800

    SpartaDictionary Project

--- app.py
+++ app.py
@@ -6,17 +6,28 @@ from flask import (
     url_for,
     jsonify
 )
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
-app = Flask(__name__)
 
-password = 'sparta'
-cxn_str = f'mongodb+srv://test:{password}@cluster0.eqimsea.mongodb.net/'
-client = MongoClient(cxn_str)
-db = client.dbsparta_plus_week2
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 
 @app.route('/')
 def main():
